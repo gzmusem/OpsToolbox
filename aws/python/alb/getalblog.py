37bf0f75a8eb6dfb0dd210a9a24016f6bf7efcbb
gzmusem <gzmusem@outlook.com>
2024-02-22 16:38:00 +0800

    添加使用说明,并且判断es中索引是否存在

--- aws/python/alb/getalblog.py
+++ aws/python/alb/getalblog.py
@@ -1,9 +1,82 @@
+"""
+使用说明:
+
+本脚本用于从指定的S3存储桶中获取Application Load Balancer(ALB)的日志，并将这些日志存储到Elasticsearch中。
+在运行此脚本之前，请确保您已经配置好AWS CLI，并且您的AWS账户有权限访问S3和Elasticsearch服务。
+
+运行环境要求:
+- Python 3.8+
+- 需要安装的库: boto3, argparse, elasticsearch
+
+参数说明:
+- bucket_name: 存储ALB日志的S3存储桶名称
+- base_prefix: S3存储桶中ALB日志的基础前缀
+- es_host: Elasticsearch服务的主机地址
+- es_index: 存储日志的Elasticsearch索引名称
+- es_user: 访问Elasticsearch服务的用户名
+- es_pass: 访问Elasticsearch服务的密码
+
+使用示例:
+python getalblog.py --bucket_name 'your_bucket_name' --base_prefix 'your_base_prefix' --es_host 'your_es_host' --es_index 'your_es_index' --es_user 'your_es_user' --es_pass 'your_es_pass'
+
+注意:
+- 请确保Elasticsearch索引已经创建，并且其映射与ALB日志的格式相匹配。
+- 此脚本默认处理当前月份的日志。如果需要处理其他月份的日志，请在base_prefix中指定相应的年份和月份。
+"""
+
+
 import boto3
 import argparse
 import gzip
 from io import BytesIO
 from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch, helpers
+
+
+def ensure_index_exists(es, es_index):
+    """
+    确保Elasticsearch索引存在，如果不存在则创建。
+    """
+    # 检查索引是否存在
+    if not es.indices.exists(index=es_index):
+        # 定义索引的映射
+        index_mappings = {
+            "mappings": {
+                "properties": {
+                    "log": {
+                        "properties": {
+                            "timestamp": {"type": "date"},
+                            "elb_status_code": {"type": "keyword"},
+                            "target_status_code": {"type": "keyword"},
+                            "request_processing_time": {"type": "float"},
+                            "target_processing_time": {"type": "float"},
+                            "response_processing_time": {"type": "float"},
+                            "elb_request_method": {"type": "keyword"},
+                            "request_url": {"type": "text"},
+                            "request_protocol": {"type": "keyword"},
+                            "user_agent": {"type": "text"},
+                            "ssl_cipher": {"type": "keyword"},
+                            "ssl_protocol": {"type": "keyword"},
+                            "target_group_arn": {"type": "keyword"},
+                            "trace_id": {"type": "keyword"},
+                            "domain_name": {"type": "keyword"},
+                            "chosen_cert_arn": {"type": "keyword"},
+                            "matched_rule_priority": {"type": "keyword"},
+                            "request_creation_time": {"type": "date"},
+                            "actions_executed": {"type": "keyword"},
+                            "redirect_url": {"type": "text"},
+                            "error_reason": {"type": "text"}
+                        }
+                    }
+                }
+            }
+        }
+        # 创建索引
+        es.indices.create(index=es_index, body=index_mappings)
+        print(f"索引 '{es_index}' 已创建。")
+    else:
+        print(f"索引 '{es_index}' 已存在。")
+
 
 def get_last_log_time(es, es_index):
     try:
@@ -24,6 +97,8 @@ def get_last_log_time(es, es_index):
 def get_alb_logs(bucket_name, base_prefix, es_host, es_index, es_user, es_pass):
     s3_client = boto3.client('s3')
     es = Elasticsearch([es_host], http_auth=(es_user, es_pass))
+
+    ensure_index_exists( es, es_index )
 
     current_year = datetime.now().year
     current_month = datetime.now().month

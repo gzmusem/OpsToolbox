f2b7b2c6c9d00ae16a542281d8110732de9421bd
gzmusem <gzmusem@outlook.com>
2024-02-23 17:30:42 +0800

    修改日志获取逻辑

--- aws/python/alb/getalblog.py
+++ aws/python/alb/getalblog.py
@@ -33,10 +33,9 @@ import json
 from io import BytesIO
 from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch, helpers
+from datetime import datetime
 
 
-
-import re
 
 def parse_log_line(line):
     pattern = re.compile(r'([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) ([^ ]*)[:-]([0-9]*) ([-.0-9]*) ([-.0-9]*) ([-.0-9]*) (|[-0-9]*) (-|[-0-9]*) ([-0-9]*) ([-0-9]*) "([^ ]*) (.*) (- |[^ ]*)" "([^"]*)" ([A-Z0-9-_]+) ([A-Za-z0-9.-]*) ([^ ]*) "([^"]*)" "([^"]*)" "([^"]*)" ([-.0-9]*) ([^ ]*) "([^"]*)" "([^"]*)" "([^ ]*)" "([^\s]+?)" "([^\s]+)" "([^ ]*)" "([^ ]*)"')
@@ -48,16 +47,16 @@ def parse_log_line(line):
             "timestamp": match.group(2),
             "elb": match.group(3),
             "client_ip": match.group(4),
-            "client_port": int(match.group(5)),
+            "client_port": match.group(5),
             "target_ip": match.group(6),
-            "target_port": int(match.group(7)),
-            "request_processing_time": float(match.group(8)),
-            "target_processing_time": float(match.group(9)),
-            "response_processing_time": float(match.group(10)),
-            "elb_status_code": int(match.group(11)),
+            "target_port": match.group(7),
+            "request_processing_time": match.group(8),
+            "target_processing_time": match.group(9),
+            "response_processing_time": match.group(10),
+            "elb_status_code": match.group(11),
             "target_status_code": match.group(12),
-            "received_bytes": int(match.group(13)),
-            "sent_bytes": int(match.group(14)),
+            "received_bytes": match.group(13),
+            "sent_bytes": match.group(14),
             "request_verb": match.group(15),
             "request_url": match.group(16),
             "request_proto": match.group(17),
@@ -82,31 +81,59 @@ def parse_log_line(line):
     else:
         return None
 
-def ensure_index_exists(es, es_index):
-    """
-    确保Elasticsearch索引存在，如果不存在则创建。
-    """
-    # 检查索引是否存在
-    if not es.indices.exists(index=es_index):
-        # 定义索引的映射
-        index_mappings = {
-            "mappings": {
-                "properties": {
-                    "log": {
-                        "type": "text"  # 确保log字段被映射为text类型
-                    },
-                    "timestamp": {
-                        "type": "date"  # 确保timestamp字段被映射为date类型
-                    }
-                    # 根据需要添加其他字段的映射
-                }
-            }
-        }
-        # 创建索引，直接传递映射参数
-        es.indices.create(index=es_index, mappings=index_mappings["mappings"])
-        print(f"索引 '{es_index}' 已创建。")
-    else:
-        print(f"索引 '{es_index}' 已存在。")
+def create_index_with_date(es, base_index_name):
+    # 获取当前日期
+    current_date = datetime.now().strftime("%Y.%m.%d")
+    
+    # 构建带有日期的索引名称
+    index_name = f"{base_index_name}-{current_date}"
+    
+    # 创建索引
+    es.indices.create(index=index_name)
+    
+    return index_name
+
+
+def get_earliest_alb_log_date(bucket_name, prefix):
+    # 创建 S3 客户端，不传递 AWS 访问密钥和密钥
+    s3_client = boto3.client('s3')
+
+    # 列出对象
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+    # 获取列表中的对象，并按创建时间排序
+    objects = response.get('Contents', [])
+    objects.sort(key=lambda x: x['LastModified'])  # 按创建时间排序
+
+    # 获取排序后列表中的第一个对象
+    first_object = objects[0] if objects else None
+    # 提取日期信息并找到最早的日期
+    earliest_date = None
+
+    if first_object:
+        key = first_object['Key']
+
+        print(key)
+
+        # 修改这里的提取方式，确保日期在正确的位置
+        yy = key.split('/')[5]  # 提取日期部分
+        mm = key.split('/')[6]
+        dd = key.split('/')[7]
+        date_components = yy + "-" + mm + "-" + dd  # 拆分年、月、日
+
+
+        # 构造日期字符串，确保年份有四位
+        date_obj = None
+
+        # 调整日期字符串构造逻辑
+        date_obj = datetime.strptime(date_components, '%Y-%m-%d')
+        print(date_obj)
+
+        if earliest_date is None or date_obj < earliest_date:
+            earliest_date = date_obj
+
+    return earliest_date
+
 
 
 def get_last_log_time(es, es_index):
@@ -125,21 +152,40 @@ def get_last_log_time(es, es_index):
         print(f"获取最后日志时间出错: {e}")
     return None
 
+
+
 def get_alb_logs(bucket_name, base_prefix, log_path_prefix, es_host, es_index, es_user, es_pass):
     s3_client = boto3.client('s3')
     es = Elasticsearch([es_host], http_auth=(es_user, es_pass))
 
-    ensure_index_exists(es, es_index)
+        # 构建索引名称
+    index_name = f"{es_index}-"
 
-    current_year = datetime.now().year
-    current_month = datetime.now().month
+    # 检查是否存在以base_index_name开头的索引
+    existing_indices = [index for index in es.indices.get('*') if index.startswith(es_index)]
+    
+    if existing_indices:
+        # 存在索引，查询最后的日志时间
+        last_log_time = get_last_log_time(es, existing_indices[-1])
+        if last_log_time:
+            print(f"Last log time in index '{existing_indices[-1]}': {last_log_time}")
+        else:
+            print("Error: Unable to get last log time.")
+    else:
+        # 不存在索引，从S3查询最早的开始时间
+        last_log_time = get_earliest_alb_log_date(bucket_name,f"{base_prefix}/{log_path_prefix}/")
+        if last_log_time:
+            # 根据最早时间创建索引
+            index_name += last_log_time.strftime("%Y.%m.%d")
+            create_index_with_date(es, index_name)
+        else:
+            print("Error: Unable to get earliest log time from S3.")
+
+
+    current_year = last_log_time.year
+    current_month = last_log_time.month
     month_prefix = f"{base_prefix}/{log_path_prefix}/{current_year}/{str(current_month).zfill(2)}/"
 
-    last_log_time = get_last_log_time(es, es_index)
-    if last_log_time:
-        print(f"Last log time: {last_log_time}")
-    else:
-        print("No previous logs found. Starting from the beginning of the month.")
 
     paginator = s3_client.get_paginator('list_objects_v2')
     page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=month_prefix)
